0034d8f9f62d30067eb820b5e2634012f490827c
peter44322 <p.pator@outlook.com>
2019-07-23 17:13:41 +0200

    fix user mode

--- src/Services/ConfigService.php
+++ src/Services/ConfigService.php
@@ -4,14 +4,14 @@
 namespace Peterzaccha\DyForm\Services;
 
 
+use Illuminate\Support\Facades\Auth;
 use Peterzaccha\DyForm\Models\DyForm;
 use Peterzaccha\DyForm\Requests\DyRequest;
 
 class ConfigService
 {
     public static function getUserInstance(DyRequest $request, DyForm $form){
-        $userModel = '\\'.config('dy-form.userModel');
-        return $userModel::find(1);
+        return Auth::user();
     }
 
     public static function getFilePath($columnName){
